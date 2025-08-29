4346ab7562cdc30c1ecaba25ea6f55d137bc024e
KHLYL01 <kk0026999@gmail.com>
2025-08-29 22:16:37 +0300

    add value to Enums

--- src/main/java/com/graduation/realestateconsulting/model/enums/HouseType.java
+++ src/main/java/com/graduation/realestateconsulting/model/enums/HouseType.java
@@ -1,5 +1,5 @@
 package com.graduation.realestateconsulting.model.enums;
 
 public enum HouseType {
-    HOME, VILLA, OFFICE, LAND, STORE, OTHER
+    HOME,APARTMENT, VILLA, OFFICE, LAND, STORE, OTHER
 }
--- src/main/java/com/graduation/realestateconsulting/model/enums/ServiceType.java
+++ src/main/java/com/graduation/realestateconsulting/model/enums/ServiceType.java
@@ -1,5 +1,5 @@
 package com.graduation.realestateconsulting.model.enums;
 
 public enum ServiceType {
-    BUY,RENT,
+    BUY,SELL,RENT,
 }
